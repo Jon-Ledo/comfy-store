be8dc7e78312895403a3e7bcfe48ad3e42434eba
JonLedo <jon.ledo@hotmail.com>
2021-11-25 12:38:41 -0500

    toggle sidebar func and about page html

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,18 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const cartOverlay = getElement('.cart-overlay')
+const closeCartBtn = getElement('.cart-close')
+const toggleCartBtn = getElement('.toggle-cart')
+
+toggleCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.add('show')
+})
+
+closeCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.remove('show')
+})
+
+// open the cart when hover over an item
+export const openCart = () => {
+  cartOverlay.classList.add('show')
+};
