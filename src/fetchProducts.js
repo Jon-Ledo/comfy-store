ae00922fdfdc1187dfeb6424bacffb2b54a65793
JonLedo <jon.ledo@hotmail.com>
2021-12-08 13:50:23 -0500

    quick update

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,11 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  const response = await fetch(allProductsUrl).catch(error => console.log("ERROR", error))
+  const data = await response.json()
+  console.log(response);
+  console.log(data);
+  return response
+};
 
 export default fetchProducts;
